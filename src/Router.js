7e4e6dca111ef21637280977e4ce4c47326d6af3
eejungee <sulau861@gmail.com>
2023-01-18 15:54:26 +0900

    오류 수정 중

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
+
+// 김한솔의 컴포넌트
+import LoginHansol from './pages/hansolKim/Login/Login';
+import MainHansol from './pages/hansolKim/Main/Main';
+
+// 박준현의 컴포넌트
+import LoginJunhyun from './pages/JunhyunPark/Login/Login';
+import MainJunhyun from './pages/JunhyunPark/Main/Main';
+
+// 성은정의 컴포넌트
+import LoginEunjung from './pages/EunjungSung/Login/Login';
+import MainEunjung from './pages/EunjungSung/Main/Main';
+
+// 신혜린님의 컴포넌트
+import LoginHeylynn from './pages/HeylynnShin/Login/Login';
+import MainHeylynn from './pages/HeylynnShin/Main/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/login-hansol" element={<LoginHansol />} />
+        <Route path="/main-hansol" element={<MainHansol />} />
+        <Route path="/login-junhyun" element={<LoginJunhyun />} />
+        <Route path="/main-junhyun" element={<MainJunhyun />} />
+        <Route path="/login-eunjung" element={<LoginEunjung />} />
+        <Route path="/main-eunjung" element={<MainEunjung />} />
+        <Route path="/login-heylynn" element={<LoginHeylynn />} />
+        <Route path="/main-heylynn" element={<MainHeylynn />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Main/Main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Main() {
-  return <h1>Hello</h1>;
-}
-
-export default Main;
