068100329e46bd11ad9d6686e47255bacc06993a
eejungee <sulau861@gmail.com>
2023-01-18 11:21:27 +0900

    초기 세팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-import Signup from "./pages/Signup/Signup";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
