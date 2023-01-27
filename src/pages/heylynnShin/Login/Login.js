7d7c20394c28ba5a4f6321e19ae563737f36712d
shinheylynn <heylub@gmail.com>
2023-01-27 21:23:12 +0900

    Add: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/heylynnShin/Login/Login.js
+++ src/pages/heylynnShin/Login/Login.js
@@ -1,4 +1,6 @@
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import './Login.scss';
 
 const LoginHeylynn = () => {
@@ -9,10 +11,25 @@ const LoginHeylynn = () => {
 
   const getUserId = event => {
     setUserId(event.target.value);
-    // console.log(event.target.value);
   };
   const getUserPw = event => {
     setUserPw(event.target.value);
+  };
+
+  const [active, setActive] = useState(false);
+
+  const passLogin = () => {
+    return userId.includes('@') && userPw.length >= 5
+      ? setActive(true)
+      : setActive(false);
+  };
+
+  const goToMain = () => {
+    if (userId.includes('@') && userPw.length >= 5) {
+      Navigate('/main-heylynn');
+    } else {
+      alert('회원 정보를 다시 입력해주세요.');
+    }
   };
 
   return (
@@ -24,12 +41,14 @@ const LoginHeylynn = () => {
         <form className="form">
           <div className="inputWrapper">
             <input
+              onKeyUp={passLogin}
               onChange={getUserId}
               id="id"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
             />
             <input
+              onKeyUp={passLogin}
               onChange={getUserPw}
               id="password"
               type="password"
@@ -37,7 +56,12 @@ const LoginHeylynn = () => {
             />
           </div>
           <div className="buttonWrapper">
-            <button type="button" className="btn">
+            <button
+              type="button"
+              className={active ? 'activeBtn' : 'btn'} // "btn"
+              onClick={goToMain}
+              disabled={userId === '' || userPw === '' ? true : false}
+            >
               <span>로그인</span>
             </button>
           </div>
