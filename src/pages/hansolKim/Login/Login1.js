5b2362bee4fbb5a17f4b10f02e8592be3e951d4a
김한솔 <gimhansol@gimhansol-ui-MacBookAir.local>
2023-01-29 03:33:22 +0900

    add: misson1,2

--- src/pages/hansolKim/Login/Login1.js
+++ src/pages/hansolKim/Login/Login1.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login1 = () => {
+  const navigate = useNavigate();
+  const navigateToMain = () => {
+    navigate('/main-hansol');
+  };
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const saveUserId = e => {
+    setId(e.target.value);
+  };
+  const saveUserPw = e => {
+    setPw(e.target.value);
+  };
+
   return (
     <>
       <div className="logo">westagram</div>
-      <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-      <input type="password" placeholder="비밀번호" />
-      <Link
-        style={{
-          textDecoration: 'none',
-          border: '1px solid',
-          width: '80%',
-          textAlign: 'center',
-          marginTop: '1rem',
-          padding: '0.5rem',
-        }}
-        to="/main-hansol"
+      <input
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+        onChange={saveUserId}
+      />
+      <input
+        name="pwtext"
+        type="password"
+        placeholder="비밀번호"
+        onChange={saveUserPw}
+      />
+      <button
+        onClick={navigateToMain}
+        disabled={!(id.includes('@') && pw.length >= 5)}
       >
         로그인
-      </Link>
+      </button>
       <div className="findPw">비밀번호를 잊으셨나요?</div>
     </>
   );
