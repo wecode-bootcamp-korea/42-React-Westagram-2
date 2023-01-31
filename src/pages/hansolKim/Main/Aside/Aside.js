0060b7ca0aaecea661c55eab0bf6a0c3b810dd5a
김한솔 <gimhansol@gimhansol-ui-MacBookAir.local>
2023-02-01 01:32:38 +0900

    add: constant data assingment1

--- src/pages/hansolKim/Main/Aside/Aside.js
+++ src/pages/hansolKim/Main/Aside/Aside.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Story from './Story';
 import Recommend from './Recommend';
 import Username from './Username';
+import { Copy_Right_List } from './data';
 import './Aside.scss';
 
 const Aside = () => {
@@ -11,8 +12,12 @@ const Aside = () => {
       <Story />
       <Recommend />
       <div className="copyright normalText">
-        Westagram 정보 . 지원 . 홍보센터 . API . 채용 정보 . 개인정보처리방침 .
-        약관 . 디렉터리 . 프로필 . 해시태그 . 언어 {'\n'} {'\n'}ⓒ 2023 WESTAGRAM
+        {Copy_Right_List.map(info => (
+          <span key={info.id}>
+            <a href="http://instagram.com">{info.listName}</a>
+          </span>
+        ))}
+        <div>2023 westagram</div>
       </div>
     </section>
   );
---
+++ src/pages/hansolKim/Main/Aside/data.js
@@ -0,0 +1,13 @@
+export const Copy_Right_List = [
+  { id: 1, listName: 'Westagram 정보 .' },
+  { id: 2, listName: '지원 .' },
+  { id: 3, listName: '홍보센터 .' },
+  { id: 4, listName: 'API .' },
+  { id: 5, listName: '채용 정보 .' },
+  { id: 6, listName: '개인정보처리방침 .' },
+  { id: 7, listName: '약관 .' },
+  { id: 8, listName: '디렉터리 .' },
+  { id: 9, listName: '프로필 .' },
+  { id: 10, listName: '해시태그 .' },
+  { id: 11, listName: '언어' },
+];
